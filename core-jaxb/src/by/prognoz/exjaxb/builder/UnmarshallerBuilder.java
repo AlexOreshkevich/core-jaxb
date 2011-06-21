a4a37731044db095398107f9600eaeadedca5664
neo.bsuir@gmail.com <neo.bsuir@gmail.com@1046067c-80da-b0a2-b2fb-b9bc7458606b>
2011-06-21 16:50:00 +0000

    Initial import

---
+++ core-jaxb/src/by/prognoz/exjaxb/builder/UnmarshallerBuilder.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.prognoz.exjaxb.builder;
+
+import by.prognoz.exjaxb.ExJAXB;
+
+/**
+ * UnmarshallerBuilder
+ * 
+ * Реализация шаблона Builder (GoF) для объектов Unmarshaller
+ * 
+ * @author oreshkevich
+ */
+public class UnmarshallerBuilder {
+  
+  public UnmarshallerBuilder(ExJAXB aThis) {
+  }
+}
